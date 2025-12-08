1ba9f9d7295f9f16ab21e549b75d3cac267a013e
Ayodeji <sulaimanbasit05@gmail.com>
2025-12-08 01:38:54 +0100

    Made functional changes and applied gas optimization techniques

--- contracts/PHASE THREE/Voting system.sol
+++ contracts/PHASE THREE/Voting system.sol
@@ -2,88 +2,103 @@
 pragma solidity ^0.8.3;
 contract VotingSystem {
     uint totalVotes;
+    uint count;
     address owner;
     struct Candidate {
         string name;
         uint voteCount;
     }
-    Candidate[] candidatesArr;
-    mapping(string => uint) CandidateIndex;
+    mapping(uint => Candidate) candidate;
+    mapping(uint => uint) idtoIndex;
     mapping(address => bool) voted;
     enum Status {Created, Ongoing, Ended}
     Status voteStatus;
+    //Custom Errors
+    error NotOwner();
+    error votingInProgress();
+    error votingInactive();
+    error NullCandidates();
+    error InsufficientVotes();
+    error InvalidVoter();
+    error AlreadyVoted();
     //Events
     event CandidateRegistered(string _name);
-    event votingStarted();
-    event votingEnded();
-    event Votecast(address indexed voter, string candidateName);
+    event votingStarted(uint timestamp);
+    event votingEnded(uint timestamp);
+    event Votecast(address indexed voter, uint candidateId);
     //Modifiers
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not Owner");
+        if(msg.sender != owner) revert NotOwner();
         _;
     }
     modifier voteOngoing() {
-        require(voteStatus == Status.Ongoing, "Voting not in progress");
+        if(voteStatus != Status.Ongoing) revert votingInactive();
         _;
     }
     modifier voteInactive() {
-        require(voteStatus != Status.Ongoing, "Voting already in progress");
+        if(voteStatus == Status.Ongoing) revert votingInProgress();
         _;
     }
     constructor() {
         owner = msg.sender;
     }
     //Admin-only actions
-    function addCandidate(string calldata _name) public onlyOwner voteInactive {
+    function addCandidate(string calldata _name, uint _id) external onlyOwner voteInactive {
         Candidate memory candidates = Candidate(_name, 0);
-        candidatesArr.push(candidates);
-        CandidateIndex[_name] = candidatesArr.length - 1;
+        candidate[count] = candidates;
+        idtoIndex[_id] = count;
+        count++;
+        // CandidateIndex[_name] = candidatesArr.length - 1;
         emit CandidateRegistered(_name);
     }
-    function startVoting() public onlyOwner voteInactive {
-        require(candidatesArr.length > 1, "Input Candidates");
+    function startVoting() external onlyOwner voteInactive {
+        if(count == 0) revert NullCandidates();
         voteStatus = Status.Ongoing;
-        emit votingStarted();
+        emit votingStarted(block.timestamp);
     }
-    function closeVoting() public onlyOwner voteOngoing{
-        require(totalVotes > 1, "Total votes should be more than 0");
+    function closeVoting() external onlyOwner voteOngoing{
+        if(totalVotes == 0) revert InsufficientVotes();
         voteStatus = Status.Ended;
-        emit votingEnded();
+        emit votingEnded(block.timestamp);
     }
     //Voting Phase
-    function vote(string calldata _name) public voteOngoing{
-        require (msg.sender != owner, "Owner cannot vote");
-        require(voted[msg.sender] == false);
-        uint index = CandidateIndex[_name];
-        candidatesArr[index].voteCount++;
+    function vote(uint _id) external voteOngoing{
+        if(msg.sender == owner) revert InvalidVoter();
+        if(voted[msg.sender]) revert AlreadyVoted();
+        uint index = idtoIndex[_id];
+        candidate[index].voteCount++;
         voted[msg.sender] = true;
         totalVotes++;
-        emit Votecast(msg.sender, _name);
+        emit Votecast(msg.sender, _id);
     }
     //Results Phase
-    function getCandidates() public voteInactive view returns(Candidate[] memory){
-        return candidatesArr;
+    function getCandidates() external voteInactive view returns(Candidate[] memory){
+        Candidate[] memory Candidates = new Candidate[](count);
+        for(uint i; i < count; ++i){
+            Candidates[i] = candidate[i];
+        }
+        return Candidates;
     }
-    function getTotalVotes() public voteInactive view returns(uint){
+    function getTotalVotes() external voteInactive view returns(uint){
         return totalVotes;
     }
-    function getVoterStatus(address _voter) public view returns(bool) {
+    function getVoterStatus(address _voter) external view returns(bool) {
         return voted[_voter];
     }
-    function getElectionState() public view returns(Status){
+    function getElectionState() external view returns(Status){
         return voteStatus;
     }
-    function getWinner() public voteInactive view returns(string memory winnerName, uint winnerVotes){
+    function getWinner() external voteInactive view returns(string memory winnerName, uint winnerVotes){
         uint highestCount;
         uint winnerIndex;
-        for(uint i = 0; i < candidatesArr.length; i++){
-            if(candidatesArr[i].voteCount > highestCount){
-                highestCount = candidatesArr[i].voteCount;
+        for(uint i; i < count; i++){
+            if(candidate[i].voteCount > highestCount){
+                highestCount = candidate[i].voteCount;
                 winnerIndex = i;
             }
         }
-        winnerName = candidatesArr[winnerIndex].name;
-        winnerVotes = candidatesArr[winnerIndex].voteCount;
+        winnerName = candidate[winnerIndex].name;
+        winnerVotes = candidate[winnerIndex].voteCount;
     }
 }
 
